43c3732700ce05a63cd8e7c0cd519515641111b1
safia88 <safialiusa@gmail.com>
2020-03-07 08:21:47 +0600

    TDD done

--- echo.py
+++ echo.py
@@ -2,21 +2,51 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Safia Ali"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument("text", help="text to be manipulated")
+    parser.add_argument(
+        "-u", "--upper", help="convert text to uppercase", action="store_true")
+    parser.add_argument(
+        "-l", "--lower", help="convert text to lowercase", action="store_true")
+    parser.add_argument(
+        "-t", "--title", help="convert text to titlecase", action="store_true")
+
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    args = create_parser().parse_args(args)
+
+    if not args:
+        create_parser().print_usage()
+        sys.exit(1)
+
+    text = args.text
+
+    if args.upper:
+        text = text.upper()
+    if args.lower:
+        text = text.lower()
+    if args.title:
+        text = text.title()
+
+    print(text)
+
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
+# python echo.py -tul "heLLo!"
+# python echo.py "Hello World" -l/-u/-t/--lower/--upper/--title
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,10 +3,75 @@
 
 import unittest
 import echo
+import subprocess
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
-    pass
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEqual(stdout, usage)
+
+    def test_parser(self):
+        """Checks if parser recognizes a given argument flag."""
+        parser = echo.create_parser()
+        args_list = ['-ult', 'hello']
+        result = parser.parse_args(args_list)
+        self.assertTrue(result.upper)
+        self.assertTrue(result.lower)
+        self.assertTrue(result.title)
+        self.assertEqual(result.text, 'hello')
+
+    def test_upper(self):
+        """Should uppercase the input text and return the text."""
+        args_list = ['-u', 'hello']
+        self.assertEqual(echo.main(args_list), 'HELLO')
+
+    def test_upper_long(self):
+        """Should uppercase the input text and return the text."""
+        args_list = ['--upper', 'hello']
+        self.assertEqual(echo.main(args_list), 'HELLO')
+
+    def test_lower(self):
+        """Should lowercase the input text and return the text."""
+        args_list = ['-l', 'HEllO']
+        self.assertEqual(echo.main(args_list), 'hello')
+
+    def test_lower_long(self):
+        """Should lowercase the input text and return the text."""
+        args_list = ['--lower', 'HEllO']
+        self.assertEqual(echo.main(args_list), 'hello')
+
+    def test_title(self):
+        """Should titlecase the input text and return the text."""
+        args_list = ['-t', 'hello']
+        self.assertEqual(echo.main(args_list), 'Hello')
+
+    def test_title_long(self):
+        """Should titlecase the input text and return the text."""
+        args_list = ['--title', 'hello']
+        self.assertEqual(echo.main(args_list), 'Hello')
+
+    def test_all(self):
+        """Should return text manipulated based on last flag as defined in parser."""
+        args_list = ['-tlu', 'hELlo']
+        self.assertEqual(echo.main(args_list), 'Hello')
+
+    def test_none(self):
+        """Should return unmanipulated text if no arguments are given."""
+        args_list = ['piero']
+        self.assertEqual(echo.main(args_list), 'piero')
+
 
 if __name__ == '__main__':
     unittest.main()
